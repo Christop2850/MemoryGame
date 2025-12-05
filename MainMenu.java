8052da69afd626cd1dbc06142e9775346e160a7e
Atiqat <aadef3@ocdsb.ca>
2025-12-05 13:14:37 -0500

    update

---
+++ MainMenu.java
@@ -0,0 +1,37 @@
+
+/**
+ * Write a description of class MainMenu here.
+ *
+ * @author (Atiqat Adefioye)
+ * @version (December 5th 2025)
+ */
+import javax.swing.JOptionPane;
+public class MainMenu
+{
+    public static void main (String args[])
+    {
+        //code an opening statement for the user
+        JOptionPane.showMessageDialog(null,"Welcome to Match Mania!!!! \nThis is a memory card game, to test your memory!!!!");
+        
+        //giving the user the rules
+        JOptionPane.showMessageDialog(null, getGameRules());
+        
+    }
+    
+    //non void method of type string to output the game rules
+    public static String getGameRules()
+    {
+        //Outputting the rules to the user
+        return ("You will be shown a grid of numbers from 1-16 to chose from"
+        + "then you will be asked to pick 2 numbers"
+        + "the numbers you have chosen will then reveal a set of letters"
+        + "if the letters you chose are the same then your score will be incremented"
+        + "but if they are not equal you will lose health!!!"
+        + "once the health reaches 0 then the game will end"
+        + "but if you get all the sets of cards before the you lose all your health then you win!!!!");
+        
+    }
+    
+    //void method for the user to sign up or login
+    
+}
